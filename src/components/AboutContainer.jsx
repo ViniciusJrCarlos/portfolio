a75d666a0ec1a59a3ee0e4182174948d2a4690d1
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-06-29 16:42:23 -0300

    enviando projeto

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -8,7 +8,7 @@ const AboutContainer = () => {
 
             <p>
 
-                Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, 
+                Lorem Ipsueeem é simplesmente uma simulação de texto da indústria tipográfica e de impressos, 
                 e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja
                 de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não 
                 só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado.
