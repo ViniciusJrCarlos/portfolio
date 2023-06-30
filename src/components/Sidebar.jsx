a72fd5d84fe32ef0fe10b3219ed2ccafb4d8f0da
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-06-30 00:39:32 -0300

    alterando sidebar do projeto

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,18 @@
-import "../styles/components/sidebar.sass";
-
-import SocialNetworks from "./SocialNetworks";
 import Avatar from "../img/perfil google.png";
 
+import "../styles/components/sidebar.sass";
+import SocialNetworks from "./SocialNetworks";
 import InformationContainer from "./InformationContainer";
-//import SocialNetworks from './SocialNetwork';
 
 const Sidebar = () => {
-
-    return (
-    
-        <aside id="sidebar">
+    return (<aside id="sidebar">
             <img src={Avatar} alt="Vinicius Carlos" />
             <p className="title">Desenvolvedor FullStack</p>
             <SocialNetworks />
             <InformationContainer />
-            <a href="#" className="btn">Download Currriculum</a>
+            <a href="#" className="btn">
+                Download Currriculum
+            </a>
         </aside>
 
         );
